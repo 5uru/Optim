1309101b0193925dab5d5a563876afa9da13454e
JoSuru <jonathansuru@icloud.com>
2023-11-05 21:25:11 +0100

    Integrate chat_manager and refine conversation handling in optim.py
    The chat_manager from optim.chat_manager is now integrated into the file. Conversations are now properly formatted and stored not only in Streamlit's session state but also in an `output.txt` file, facilitating subsequent process. Role labels "assistant" and "user" have been changed to "Agent" and "Patient" respectively for better context understanding.

--- optim.py
+++ optim.py
@@ -2,18 +2,26 @@
 
 import streamlit as st
 
-st.set_page_config(page_title=' Optim Assistant', layout='wide')
+from optim.chat_manager import main as chat_manager
+
+st.set_page_config(page_title=" Optim Assistant", layout="wide")
+
 
 st.title("💬 Chatbot")
 if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+    st.session_state["messages"] = [{"role": "Agent", "content": "How can I help you?"}]
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
 if prompt := st.chat_input():
-    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.session_state.messages.append({"role": "Patient", "content": prompt})
     st.chat_message("user").write(prompt)
-
-conversation = st.session_state["messages"]
-st.write(conversation)
+    conversation = " "
+    for msg in st.session_state.messages:
+        conversation += "\n\n\n" + msg["role"] + ": " + msg["content"]
+    with open("output.txt", "w") as f:
+        f.write(conversation)
+    response = chat_manager(conversation)
+    st.session_state.messages.append({"role": "Agent", "content": response})
+    st.chat_message("assistant").write(response)
