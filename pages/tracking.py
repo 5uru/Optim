5982d27c0b0327003daf5aaa1eab542cf573cadb
JoSuru <jonathansuru@icloud.com>
2023-11-09 21:03:47 +0100

    Add temperature metric and data table to tracking page in Streamlit

--- pages/tracking.py
+++ pages/tracking.py
@@ -1,5 +1,38 @@
 # Import necessary  libraries
 
+import random
+
+import pandas as pd
 import streamlit as st
 
 st.set_page_config(page_title=" Optim Tracking", layout="wide")
+
+with st.container():
+    st.metric("Temperature", "70 °F", "1.2 °F")
+    st.write("The temperature is above the recommended level.")
+
+df = pd.DataFrame(
+    {
+        "name": ["Roadmap", "Extras", "Issues"],
+        "url": ["https://roadmap.streamlit.app", "https://extras.streamlit.app", "https://issues.streamlit.app"],
+        "stars": [random.randint(0, 1000) for _ in range(3)],
+        "views_history": [[random.randint(0, 5000) for _ in range(30)] for _ in range(3)],
+    }
+)
+st.dataframe(
+    df,
+    column_config={
+        "name": "App name",
+        "stars": st.column_config.NumberColumn(
+            "Github Stars",
+            help="Number of stars on GitHub",
+            format="%d ⭐",
+        ),
+        "url": st.column_config.LinkColumn("App URL"),
+        "views_history": st.column_config.LineChartColumn(
+            "Views (past 30 days)", y_min=0, y_max=5000
+        ),
+    },
+    hide_index=True,
+    use_container_width=True
+)
