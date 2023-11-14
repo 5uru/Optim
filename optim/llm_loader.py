e92683fd63e03973c0bea8cd1cba0494bb5cc5be
Jonathan Suru <jonathansuru@icloud.com>
2023-11-14 20:19:48 +0100

    Integrate new chat rendering feature in chat_manager.py

--- optim/llm_loader.py
+++ optim/llm_loader.py
@@ -4,7 +4,6 @@ from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.llms import Ollama
 from langchain.llms import OpenAI
 
-
 # Load the configuration
 with initialize(config_path="../conf", version_base="1.3"):
     cfg = compose(config_name="config.yaml")
@@ -19,7 +18,7 @@ def main():
     if cfg.llm.name == "ollama":
         llm = Ollama(
             model=cfg.llm.model_name,
-            verbose=False,
+            verbose=True,
             callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]),
         )
         # Return the LLM
