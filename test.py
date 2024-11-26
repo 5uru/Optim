c168c92d9fb4c0e223360198d66ab572ddab0a95
Jonathan Suru <jonathansuru@icloud.com>
2024-11-26 20:24:49 +0100

    refactor: remove  all file

--- test.py
+++
@@ -1,9 +0,0 @@
-# open conversations/11.json
-import json
-
-from optim.extraction import main as extract
-
-with open("./conversations/11.json", "r") as f:
-    conversation = json.load(f)
-
-print(extract(conversation["messages"]))
